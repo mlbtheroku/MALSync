e183dfb822e0feac72e5541dd824b417e77f7cc2
mlbtheroku <mlbtheroku@users.noreply.github.com>
2023-07-29 05:07:13 +0000

    Auto Urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -641,6 +641,7 @@ module.exports = {
       '*://sizyreelingly.com/e/*',
       '*://rationalityaloelike.com/e/*',
       '*://wolfdyslectic.com/e/*',
+      '*://gamoneinterrupted.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
